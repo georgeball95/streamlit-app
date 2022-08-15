7014ebf90dd2e1a91cb18ec88b80f650890f2ed1
George Ball <georgeball95@gmail.com>
2022-08-15 08:14:29 +0100

    Update streamlit_app.py
    Added brier score

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,8 @@ st.header("1) Chance that Erling Haaland will outscore Mo Salah this weekend?")
 
 prediction1 = st.text_input("% chance prediction")
 
+outcome = st.text_input("outcome")
+
+brier = (prediction-outcome)**2
+
+st.markdown("your brier score: {}".format(brier))
